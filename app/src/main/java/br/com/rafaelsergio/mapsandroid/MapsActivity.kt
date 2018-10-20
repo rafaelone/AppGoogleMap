5d5c832d818b3426d91840f44900a8624e5147ff
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-20 12:24:47 -0300

    adicionando configurações ao mapa

--- app/src/main/java/br/com/rafaelsergio/mapsandroid/MapsActivity.kt
+++ app/src/main/java/br/com/rafaelsergio/mapsandroid/MapsActivity.kt
@@ -1,14 +1,25 @@
 package br.com.rafaelsergio.mapsandroid
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import br.com.rafaelsergio.mapsandroid.utils.PermissaoUtils
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
@@ -18,30 +29,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    val permissoesLocalizacao = listOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    private lateinit var locaionManager: LocationManager
+    private lateinit var locationListener: LocationListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        PermissaoUtils.validaPermissao(permissoesLocalizacao.toTypedArray(), this, 1)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun initLocationListener(){
+       locationListener = object :LocationListener{
+           override fun onLocationChanged(location: Location?) {
+               val minhaPosicao = LatLng(location?.latitude!!, location?.longitude)
+               addMarcador(minhaPosicao, "Mâe to no maps")
+               mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(minhaPosicao, 12f))
+           }
+
+           override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+
+           }
+
+           override fun onProviderEnabled(p0: String?) {
+           }
+
+           override fun onProviderDisabled(p0: String?) {
+           }
+
+       }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for(resposta in grantResults){
+            if(resposta == PackageManager.PERMISSION_DENIED){
+                Toast.makeText(applicationContext, " Sem permissao sem acesso", Toast.LENGTH_LONG).show()
+            }else{
+              requestLocationUpdates()
+
+            }
+        }
+
+    }
+
+    private fun requestLocationUpdates(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        }
+    }
+
+
     private fun addMarcador(latLng: LatLng, titulo: String) {
         mMap.addMarker(MarkerOptions().position(latLng).title(titulo))
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        initLocationListener()
+        requestLocationUpdates()
 
         val fiapPaulista = LatLng(-23.563814, -46.652442)
         val fiapAclimacao = LatLng(-23.571913, -46.623336)
@@ -73,7 +128,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title("Fiap Vila Olimpia")
                 .snippet(getEnderecoFormatado(fiapVilaOlimpia)))
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fiapPaulista, 12f))
+        val circulo = CircleOptions()
+        circulo.center(fiapPaulista)
+        circulo.radius(200.0)
+        circulo.fillColor(Color.argb(128, 0, 51,102))
+        circulo.strokeWidth(1f)
+        mMap.addCircle(circulo)
 
     }
 
